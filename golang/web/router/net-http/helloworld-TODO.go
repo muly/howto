7de2d62bcb410613e1cbfea9af7df51d331af921
Praveen Kumar K <30298737+pk80@users.noreply.github.com>
2017-08-01 23:30:09 +0530

    Update helloworld-TODO.go

--- golang/web/router/net-http/helloworld-TODO.go
+++ golang/web/router/net-http/helloworld-TODO.go
@@ -1,1 +1,18 @@
 // basic hello world web application just using net/http package
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type clientRequest int
+
+func (c clientRequest) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintln(w, "Hi client request")
+}
+
+func main()  {
+	var c1 clientRequest
+	http.ListenAndServe(":8080",c1)
+}
