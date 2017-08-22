28ab91259617e24a34a2bb762f8030ef461ed8d8
Praveen <praveen.goprog@gamil.com>
2017-08-22 19:14:26 +0530

    basic helloworld webapp using net/http package

--- golang/web/router/net-http/helloworld-TODO.go
+++ golang/web/router/net-http/helloworld-TODO.go
@@ -1,1 +1,16 @@
 // basic hello world web application just using net/http package
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func hellworld(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w,"Welcome to HelloWorld...!")
+}
+
+func main()  {
+	http.HandleFunc("/",hellworld)
+	http.ListenAndServe(":8080",nil)
+}
