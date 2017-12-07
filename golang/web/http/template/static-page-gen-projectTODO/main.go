0d62293b66347bc683d14cabaea1690bfa2c2108
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-12-07 10:51:31 -0800

    Create main.go

---
+++ golang/web/http/template/static-page-gen-projectTODO/main.go
@@ -0,0 +1,6 @@
+// given an template layout file and some html text, use standard library templates to bund these two and return a html content
+// write a function to do the above.
+// func htmlPageGen(layoutFile, htmlContent)htmlData{}
+// choose a template layout file that has a header section, footer section, side navigation bar, and area at the centre of the page to hold the dynamic content 
+//
+
