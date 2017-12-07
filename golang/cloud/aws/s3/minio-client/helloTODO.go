78670766610d0abf26566fa04e6051fc82af6c51
Srinivasa R Muly <msrinivasareddy@gmail.com>
2017-12-07 10:45:03 -0800

    Create helloTODO.go

---
+++ golang/cloud/aws/s3/minio-client/helloTODO.go
@@ -0,0 +1,5 @@
+//TODO: use the minio API to connect to Amazon S3 to read and write to a S3 bucket on AWS. 
+
+// Ref:
+// Minio Go Client SDK for Amazon S3 Compatible Cloud Storage
+// https://docs.minio.io/docs/golang-client-quickstart-guide
