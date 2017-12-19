29df3460823e484055b6410e6a8c2cbf4909c0fb
muly <msrinivasareddy@gmail.com>
2017-12-18 16:04:45 -0800

    sending and receiving file using form data file type: client and server example

--- golang/web/http/client/net-http/post-request-body-types-TODO.go
+++ golang/web/http/client/net-http/post-request-body-types-TODO.go
@@ -1,6 +1,82 @@
-send the client request with data in body. give examples of sending data in differnt formats listed below:
-1) form data (types: text, file)
+/*send the client request with data in body. give examples of sending data in differnt formats listed below:
+//1) form data (2 types: (1a) text, (1b) file)
 2) x-www-form-urlencoded
 3) raw data: json or any plain text formats
 4) binary
-5) multipart file attachment?????
+5) multipart file attachment????? */
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	filename := `C:\gows\src\github.com\muly\howto\golang\web\http\client\net-http\post-request-body-types-TODO.go`
+	url := "http://localhost:8080/formdatafile"
+	err := sendFileAsMultipartFileAttachment(filename, url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// 1b: form data file type client example
+func sendFileAsMultipartFileAttachment(filename string, serverUrl string) error {
+
+	body, contentType, err := GetMultipartFormData(filename)
+	if err != nil {
+		fmt.Println("GetMultipartFormData error:", err)
+		return err
+	}
+
+	resp, err := http.Post(serverUrl, contentType, body)
+	if err != nil {
+		fmt.Println("cannot post", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	byte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("cannot read the responce", err)
+		return err
+	}
+	fmt.Println(string(byte))
+
+	return nil
+}
+
+func GetMultipartFormData(filename string) (data io.Reader, contentType string, err error) {
+
+	body := &bytes.Buffer{}
+	mw := multipart.NewWriter(body)
+
+	w, err := mw.CreateFormFile("file", filename)
+	if err != nil {
+		return nil, "", err
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, "", err
+	}
+	_, err = io.Copy(w, f)
+	if err != nil {
+		return nil, "", err
+	}
+
+	err = mw.Close()
+	if err != nil {
+		return nil, "", err
+	}
+
+	return body, mw.FormDataContentType(), nil
+}
--- golang/web/http/server/net-http/post-request-body-types-TODO.go
+++ golang/web/http/server/net-http/post-request-body-types-TODO.go
@@ -1,15 +1,16 @@
 //accept the request to server with data in body in various ways listed below:
-//1) form data (types: text, file)
+//1) form data (2 types: (1a) text, (1b) file)
 //2) x-www-form-urlencoded
 //3) raw data: json or any plain text formats
 //4) binary
 //5) multipart file attachment?????
-package net_http
+package main
 
 import (
 	"fmt"
-	"net/http"
 	"io"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
@@ -17,16 +18,16 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-//1b) form data file type example-TODO
+//1b) form data file type server example
 func handlerFormDataFileType(w http.ResponseWriter, r *http.Request) {
 
-	r.ParseMultipartForm(32<<30)
+	r.ParseMultipartForm(32 << 30)
 	dataFile, _, err := r.FormFile("file") // Here file is the key specified in the form while sending the file in form
-	if err != nil{
+	if err != nil {
 		fmt.Fprintln(w, "r.formfile error:"+err.Error())
 		return
 	}
-	if dataFile == nil{
+	if dataFile == nil {
 		fmt.Fprintln(w, "file NOT received")
 		return
 	}
@@ -37,6 +38,12 @@ func handlerFormDataFileType(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "file received")
 }
 
-func processFile(f io.Reader)error{
+func processFile(f io.Reader) error {
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(b))
 	return nil
 }
