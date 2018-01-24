40e5573750168fe85ca8896852c279d701842cb3
muly <msrinivasareddy@gmail.com>
2018-01-24 15:30:37 -0800

    rpc example with multiple methods and with reply (return) parameter

---
+++ golang/web/rpc/example-with-multiple-methods/rpc-client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+)
+
+func main() {
+	c, err := rpc.Dial("tcp", "127.0.0.1:8080") // assuming server on localhost with this port
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	//Note: match the data type with the server function's parameters
+	err = c.Call("MyServer.Hello", 0, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} 
+	fmt.Println("server Hello function executed")
+
+	out :="" 
+	err = c.Call("MyServer.Echo", "Good morning", &out)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} 
+	fmt.Println("server Echo function executed. received:", out)
+}
---
+++ golang/web/rpc/example-with-multiple-methods/rpc-server.go
@@ -0,0 +1,48 @@
+// Note: inspired from the example https://github.com/kmanley/golang-rpc-example
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+)
+
+type MyServer struct{}
+
+// properties of the correct rpc function:
+// all rpc functions must have 3 input params: receiver, *args, *reply
+// 	first input parameter is the receiver itself
+// 	third input parameter must be pointer.
+// and one output parameter, and its return type must be error
+
+func (MyServer) Hello(args int64, reply *int64)error{
+	fmt.Println("Hello World")
+	return nil
+}
+
+func (MyServer) Echo(args string, reply *string)error{
+	fmt.Println("received & echoed:", args)
+	*reply = args
+	return nil
+}
+
+func main(){
+	err := rpc.Register(new(MyServer))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
+}
+
