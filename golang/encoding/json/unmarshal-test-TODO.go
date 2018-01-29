7d63af64e0ff0c1ba801dd6e6c5d59a7830f4aa8
Srinivasa R Muly <msrinivasareddy@gmail.com>
2018-01-29 09:59:24 -0800

    Create unmarshal-test-TODO.go

---
+++ golang/encoding/json/unmarshal-test-TODO.go
@@ -0,0 +1,3 @@
+// demonstrate unit testing for json unmarshal
+// test case format: json string, and expected struct value after unmarshal
+// reference: unit testing basics: https://blog.alexellis.io/golang-writing-unit-tests/
