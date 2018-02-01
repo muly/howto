decb692d7300cc75e3f6abece49a1d358349cb05
muly <msrinivasareddy@gmail.com>
2018-02-01 10:58:23 -0800

    struct-with-slice more cases

--- golang/util/equal/struct/struct-with-slice.go
+++ golang/util/equal/struct/struct-with-slice.go
@@ -14,7 +14,15 @@ func main() {
 
 	s1 := st{a: 1, b: []string{"hello"}}
 	s2 := st{a: 1, b: []string{"hello"}}
-
 	//fmt.Println(s1 == s2) // invalid operation: s1 == s2 (struct containing []string cannot be compared)
-	fmt.Println(reflect.DeepEqual(s1, s2))
+	fmt.Println(reflect.DeepEqual(s1, s2)) // true
+
+	s3 := st{a: 1, b: []string{"hello", "world"}}
+	s4 := st{a: 1, b: []string{"hello", "world"}}
+	fmt.Println(reflect.DeepEqual(s3, s4)) // true
+
+	s5 := st{a: 1, b: []string{"hello", "world"}}
+	s6 := st{a: 1, b: []string{"world", "hello"}}
+	fmt.Println(reflect.DeepEqual(s5, s6)) // false
+
 }
