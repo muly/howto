3ad227a5650d955d7eab54abdeb2b722a4cea762
muly <msrinivasareddy@gmail.com>
2018-03-15 17:57:45 -0700

    lambda function trigger with API gateway

--- golang/cloud/aws/lambda/hello-lambda.go
+++ golang/cloud/aws/lambda/hello-lambda.go
@@ -5,6 +5,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -13,26 +15,36 @@ type Req struct {
 	Value string `json:"value"`
 }
 
-type Resp struct {
-	Message string `json:"message"`
-	Ok      bool   `json:"ok"`
+// type Resp struct {
+// 	Message string `json:"message"`
+// 	Ok      bool   `json:"ok"`
+// }
+
+type GateWayResp struct { //Note: if the lambda is triggered via API Gateway then the response needs to be in below format, otherwise, the request would fail eventhough the Lambda actually succeeded
+	IsBase64Encoded bool `json:"isBase64Encoded"`
+	StatusCode      int  `json:"statusCode"`
+	//Headers `json:"headers"`
+	Body string `json:"body"`
 }
 
-func Handler(req Req) (resp Resp, err error) {
-	return Resp{
-		Message: fmt.Sprintf("processing the request ID %v", req.ID),
-		Ok:      true,
+func Handler(req Req) (resp GateWayResp, err error) {
+	return GateWayResp{
+		IsBase64Encoded: false,
+		StatusCode:      http.StatusOK,
+		Body:            fmt.Sprintf("processing the request ID %v", req.ID),
+
+		//Message: fmt.Sprintf("processing the request ID %v", req.ID),
+		//Ok:      true,
 	}, nil
 }
 
 func main() {
 	lambda.Start(Handler)
-	// r, err:= Handler(Req{ID:1, Value:"hello"})	
+	// r, err:= Handler(Req{ID:1, Value:"hello"})
 	// if err != nil{
 	// 	fmt.Println(err)
 	// 	return
 	// }
 	// fmt.Println(r)
-	
 
 }
