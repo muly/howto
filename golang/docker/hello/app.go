19f7dd14e2039a3ec4f730d308247aa40c3827f3
muly <msrinivasareddy@gmail.com>
2018-03-22 15:34:00 -0700

    docker: golang hello world app

---
+++ golang/docker/hello/app.go
@@ -0,0 +1,18 @@
+// demonstrate a simple app with docker
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// docker build to generate image, and tag it with the specified tag (using -t flag in the below example)
+// 		docker build -t helloapp .
+// docker run to create the container from the specified image (using tag in below example)
+// 		docker run helloapp
+//
+//
