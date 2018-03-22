f53272a7445730579285f759be66ed3f1cf6c4a7
muly <msrinivasareddy@gmail.com>
2018-03-22 16:41:07 -0700

    docker example with a go app with small docker image size

---
+++ golang/docker/hello-small/app.go
@@ -0,0 +1,23 @@
+// demonstrate a simple app with docker (with small image size)
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
+
+//// multi-step container build:
+// compile go code and add the binary into the container
+//		CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
+// docker build to generate image, and tag it with the specified tag (using -t flag in the below example)
+//		docker build -t hellosmallapp -f Dockerfile .
+// docker run to create the container from the specified image (using tag in below example)
+// 		docker run hellosmallapp
+
+// https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/ #Part 3: Compile!
+// vs
+// https://medium.com/travis-on-docker/how-to-dockerize-your-go-golang-app-542af15c27a2 #Way 2: Build Outside your Dockerfile
