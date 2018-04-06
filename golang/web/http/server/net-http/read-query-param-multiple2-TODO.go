41af7cae545513e19ec6511f903214eeb3506812
muly <msrinivasareddy@gmail.com>
2018-04-06 14:44:12 -0700

    sol for read-query-param-multiple2

--- golang/web/http/server/net-http/read-query-param-multiple2-TODO.go
+++ golang/web/http/server/net-http/read-query-param-multiple2-TODO.go
@@ -1,4 +1,33 @@
-// example to demonstrate reading query parameters (with multiple values in the below format) using net/http library. 
+// example to demonstrate reading query parameters (with multiple values in the below format) using net/http library.
 // example input: https://localhost:8080/customer?country=usa&country=india
 // required output: a slice of string with values usa and india like this []string{"usa","india"}
-// 
+//
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+func queryParamHandler(w http.ResponseWriter, r *http.Request) {
+	vars, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	var country []string
+	country = vars["country"]
+
+	fmt.Fprintln(w, country)
+}
+
+func main() {
+
+	http.HandleFunc("/customer", queryParamHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
