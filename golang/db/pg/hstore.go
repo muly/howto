ea0a547f61d77bd697199d334cdc5c66ef3fec85
pk80 <praveen.goprog@gmail.com>
2018-04-24 13:56:10 -0700

    hstore example

---
+++ golang/db/pg/hstore.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"database/sql"
+
+	_ "github.com/lib/pq"
+	"github.com/lib/pq/hstore"
+)
+
+var db *sql.DB
+
+func init(){
+	var err error
+	dburl := os.Getenv("dbserver")
+	db, err = sql.Open("postgres", dburl)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+}
+//
+
+func main(){
+	rows, err := db.Query("select attributes from shard_1.user where id = 123") 
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		attr, test := hstore.Hstore{}, 10
+		err= rows.Scan(&attr)
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+		for k, v:= range attr.Map{
+			fmt.Printf("%v:%v\n", k, v.String)
+		}
+	}		
+}
