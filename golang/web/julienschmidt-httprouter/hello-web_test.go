742371ffebb646f2500fdc29cecdb5a945ad9f3c
muly <msrinivasareddy@gmail.com>
2018-05-01 15:48:11 -0700

    httprouter hello server and test handler

---
+++ golang/web/julienschmidt-httprouter/hello-web_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Test_helloHandler(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/hello", helloHandler)
+	// router.Handle("GET", "/hello", helloHandler)
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	router.ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Errorf("Wrong status: wanted %v, but got %v", http.StatusOK, w.Code)
+	}
+}
