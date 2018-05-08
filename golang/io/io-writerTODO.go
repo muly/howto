60f609d23ef026b98a92eb84125d9b755e57ef86
muly <msrinivasareddy@gmail.com>
2018-05-07 17:55:38 -0700

    io writer

---
+++ golang/io/io-writerTODO.go
@@ -0,0 +1,23 @@
+// to demonstrate how to open a writer and save a file to writer instead of to disk
+
+package main
+
+import(
+
+)
+
+func main(){
+	..
+
+	write2Disk(f)
+
+	write2Writer()
+}
+
+func write2Disk(){
+
+}
+
+func write2Writer(){}
+
+
