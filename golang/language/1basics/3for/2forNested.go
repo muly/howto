fc02968227f70e9252908c63dcd54a9fe4d1445f
muly <msrinivasareddy@gmail.com>
2018-05-30 05:49:15 -0700

    language basics: for, array

---
+++ golang/language/1basics/3for/2forNested.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main(){
+	forOutside:
+	for i:=0; i<5;i++{
+		for j:= 0; j<5; j++{
+			if j> 2{
+				break forOutside // will break to outer loop, otherwise breal will only break from innermost for loop, where that break is called
+			}
+			fmt.Println(i, j)
+		}
+	}
+}
