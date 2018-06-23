b578f6c12f0b24d586636995a5aabe8700c25781
muly <msrinivasareddy@gmail.com>
2018-06-23 07:07:20 -0700

    cleanup

--- golang/language/1basics/1var_datatypes/3datatypes.go
+++ golang/language/1basics/1var_datatypes/3datatypes.go
@@ -8,8 +8,9 @@ func main() {
 	var i int = 1       // int int8 int16 int32 int64
 	var f float32 = 1.1 // float32 float64
 	var s string = "hello"
+	var b bool = true
 
-	fmt.Printf("%d %f %s\n", i, f, s)
+	fmt.Printf("%d %f %s %v\n", i, f, s, b)
 }
 
 
