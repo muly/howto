3f45ec539f522c37177e9f6759b27a314e4aa630
muly <msrinivasareddy@gmail.com>
2018-06-26 16:05:07 -0700

    zip and maps

---
+++ golang/archive/zip/zip-handler-filesTODO.go
@@ -0,0 +1,4 @@
+// demonstrate creating a zip file from a static list of files available on the server
+
+package main
+
---
+++ golang/archive/zip/zip-handler-text.go
@@ -0,0 +1,42 @@
+// demonstrate creating a zip file with a map of filename string to []bytes as input data
+
+package main
+
+import (
+	"archive/zip"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", zipit)
+	http.ListenAndServe(":8080", nil)
+}
+
+func zipit(w http.ResponseWriter, r *http.Request) {
+	// Create a new zip archive
+	writer := zip.NewWriter(w)
+
+	// Add some files to the archive.
+	files := map[string][]byte{}
+	
+	files["readme.txt"]= []byte("This archive contains some text files.")
+	files["gopher.txt"]= []byte("Gopher names:\nGeorge\nGeoffrey\nGonzo")
+	files["todo.txt"]= []byte("Get animal handling licence.\nWrite more examples.")
+	
+	for fileName, fileBody:= range files {
+		f, err := writer.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = f.Write(fileBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}	
+	// Make sure to check the error on Close.
+	err := writer.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
