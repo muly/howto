b9a2ae52e06ee241f2c9820bab829444bd146271
muly <msrinivasareddy@gmail.com>
2018-07-17 16:36:15 -0700

    gopacket basic examples from web

---
+++ golang/nw/gopacket/find-devices.go
@@ -0,0 +1,32 @@
+// find network devices on the computer along with the IP address
+
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket/pcap"
+	"log"
+)
+
+func main() {
+	// Find all devices
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print device information
+	fmt.Println("Devices found:")
+	for _, device := range devices {
+		fmt.Println("\nName: ", device.Name)
+		fmt.Println("Description: ", device.Description)
+		fmt.Println("Devices addresses: ", device.Description)
+		for _, address := range device.Addresses {
+			fmt.Println("- IP address: ", address.IP)
+			fmt.Println("- Subnet mask: ", address.Netmask)
+		}
+	}
+}
+
+
+// SOurce: https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
