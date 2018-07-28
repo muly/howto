930ae15b1e7b6b34a6a8558aaefb31239df886c0
Srinivasa R Muly <msrinivasareddy@gmail.com>
2018-07-28 07:48:13 -0700

    Create 5slice-append-TODO.go

---
+++ golang/language/2datastructures/2slice/5slice-append-TODO.go
@@ -0,0 +1,3 @@
+// demonstrate the append a slice covering the below cases:
+// case 1: append resulting in the same underlying array
+// case 2: append resulting in a different underlying array (capacity grows too)
