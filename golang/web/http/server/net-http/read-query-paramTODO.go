395dd8db94a612e9d437de7711748e26175258db
Srinivasa R Muly <msrinivasareddy@gmail.com>
2018-08-01 17:33:25 -0700

    Update and rename read-query-param.go to read-query-paramTODO.go

--- golang/web/http/server/net-http/read-query-paramTODO.go
+++ golang/web/http/server/net-http/read-query-paramTODO.go
@@ -1,5 +1,5 @@
 //example to demonstrate reading query parameters using net/http library. 
-// example: https://localhost:8080/?country=usa
+// example: http://localhost:8080/?country=usa&state=ca
 
 package main
 
@@ -9,16 +9,26 @@ import (
 )
 
 func queryParamHandler(w http.ResponseWriter, r *http.Request) {
+	// method 1: 
 	vars, err := url.ParseQuery(r.URL.RawQuery)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
-
 	country := ""
 	if val, exists := vars["country"]; exists {
 		country = val[0]
 	}
-	w.Write([]byte(country))
+	
+	// method 2: 
+	vars2 := r.URL.Query()
+	state := ""
+	if val, exists := vars2["state"]; exists {
+		state = val[0]
+	}
+	
+	//TODO: any other methods? from other libraries, which support without having to worry about index (like vars[0])
+	
+	w.Write([]byte(country+","+state))
 }
 
 func main() {
