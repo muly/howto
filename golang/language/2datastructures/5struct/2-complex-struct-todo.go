8194b359ad6424468b5cfab020faa69fa7025c4c
muly <msrinivasareddy@gmail.com>
2018-09-15 15:32:31 -0700

    struct some more todos addressed

--- golang/language/2datastructures/5struct/2-complex-struct-todo.go
+++ golang/language/2datastructures/5struct/2-complex-struct-todo.go
@@ -1,22 +1,30 @@
 // struct fields can have complex datatype
-//TODO: need to add more (self explanatory) notes here
 package main
 
 import (
 	"fmt"
 )
 
+type address struct {
+	line1, line2              string
+	city, state, zip, country string
+}
+
 type customer struct {
 	fname, lname string
 	emails       []string
+	alsoEmails   map[string]string
+	homeAddress  address
 }
 
 func main() {
 
 	c := customer{}
-	c.fname = "F"
-	c.lname = "L"
-	c.emails = []string{"email1", "email2"}
+	c.fname = "abc"
+	c.lname = "xyz"
+	c.emails = []string{"abc@email.com", "email2"}
+	c.alsoEmails = map[string]string{"email": "abc@email.com"}
+	c.homeAddress = address{city: "SFO", country: "usa"}
 
 	fmt.Println(c)
 
---
+++ golang/language/2datastructures/5struct/4-sctruct-embedding-TODO.go
@@ -0,0 +1,5 @@
+//TODO: demonstrate struct embedding, some cases to cover
+// embedding with field name
+// embedding without field name
+
+package main
