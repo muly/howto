bf9ede8f20568c3961d7e6087dc466e93458290b
Srinivasa R Muly <msrinivasareddy@gmail.com>
2019-01-18 01:50:08 -0500

    Update read-query-param-multiple2.go

--- golang/web/http/server/net-http/read-query-param-multiple2.go
+++ golang/web/http/server/net-http/read-query-param-multiple2.go
@@ -1,5 +1,5 @@
 // example to demonstrate reading query parameters (with multiple values in the below format) using net/http library.
-// example input: https://localhost:8080/customer?country=usa&country=india
+// example input: http://localhost:8080/customer?country=usa&country=india
 // required output: a slice of string with values usa and india like this []string{"usa","india"}
 //
 
