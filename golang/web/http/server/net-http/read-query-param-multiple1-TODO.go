7572ec05109934d315aa3a589f9225d41ed664c3
Srinivasa R Muly <msrinivasareddy@gmail.com>
2019-01-18 02:01:10 -0500

    Update and rename read-query-param-multiple1-TODO.go to read-query-param-multiple1.go

--- golang/web/http/server/net-http/read-query-param-multiple1-TODO.go
+++
@@ -1,4 +0,0 @@
-// example to demonstrate reading query parameters (with multiple values in the below format) using net/http library. 
-// example input: http://localhost:8080/customer?country=usa,india
-// required output: a slice of string with values usa and india like this []string{"usa","india"}
-// 
