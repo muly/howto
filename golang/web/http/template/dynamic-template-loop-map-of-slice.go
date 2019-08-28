8fdca647d35919fdf4c6183e0e188cbc42ef12e9
Srinivasa R Muly <msrinivasareddy@gmail.com>
2019-08-28 18:47:28 -0400

    Update dynamic-template-loop-map-of-slice.go

--- golang/web/http/template/dynamic-template-loop-map-of-slice.go
+++ golang/web/http/template/dynamic-template-loop-map-of-slice.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"net/http"
 	"text/template"
 )
 
@@ -37,8 +36,8 @@ func main() {
 	p2 := person{"Srinivasa", "Reddy", "M", 36}
 	p3 := person{"Mahesh", "Reddy", "M", 36}
 	persons := map[string][]person{}
-	persons["tenant1"]=[]person{p1,p2}
-	persons["tenant2"]=[]person{p3}
+	persons["tenant1"] = []person{p1, p2}
+	persons["tenant2"] = []person{p3}
 
 	tmpl, err := template.New("dynamic-loop").Parse(dtmpl)
 	if err != nil {
@@ -48,7 +47,7 @@ func main() {
 	//var tpl bytes.Buffer
 	//tmpl.Execute(&tpl, persons)
 	//fmt.Println(tpl.String())
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, persons)
 	})
 	http.ListenAndServe(":8080", nil)
