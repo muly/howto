4a69f5a8338378f1e0d64be28f1dfb96c37ff01b
muly <msrinivasareddy@gmail.com>
2020-08-02 19:14:12 -0400

    Revert "gorilla mux parsing url params and query params examples"
    This reverts commit 75a6f1a88106e2512b5244797572bed112f37a3a.

--- golang/web/http/server/gorillamux/read-url-paramTODO.go
+++ golang/web/http/server/gorillamux/read-url-paramTODO.go
@@ -1,26 +1,27 @@
 // example to demonstrate how to read parameter from the url. example id (1) from the below
-// http://localhost:8080/customer/{id}
-// http://localhost:8080/customer/1
+// https://localhost:8080/customer/{id}
+// https://localhost:8080/customer/1
+
+
+//TODO: test the handler
 
 package main
 
 import (
-	"fmt"
 	"net/http"
-
+  
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	h := mux.NewRouter()
-	h.HandleFunc("/customer/{id}", handler)
-	http.ListenAndServe(":8080", h)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	fmt.Fprintf(w, "received 
-	id = %v", id)
+	
+	fmt.Fprintf(w, "received id = %v", id)
 }
