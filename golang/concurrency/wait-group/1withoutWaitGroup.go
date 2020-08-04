45721632e37049f78b3118d04fa84e7f80eda905
muly <msrinivasareddy@gmail.com>
2020-08-04 18:37:00 -0400

    wait group example

---
+++ golang/concurrency/wait-group/1withoutWaitGroup.go
@@ -0,0 +1,23 @@
+// demonstrate the problems because of lack of synching mechanism like wait groups
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go msg(i)
+	}
+}
+
+func msg(i int) {
+	fmt.Println(i)
+}
+
+
+// Output: all the numbers will not be printed 
+// because the go routines will not have a chance to execute before the main function ends
+// you need to introduce some sort of delay at the end of the main function so that it waits for some time before exiting.
+// this can be achieved using wait group as shown in next example "2withWaitGroup.go"
---
+++ golang/concurrency/wait-group/2withWaitGroup.go
@@ -0,0 +1,28 @@
+// demonstrate the use of wait groups
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go msg(i)
+	}
+	wg.Wait()
+}
+
+func msg(i int) {
+	fmt.Println(i)
+	wg.Done()
+}
+
+
+
+// Output: all the numbers will be printed 
+// because, using wait group, we are waiting for all the go routines to complete execuing before the main function exit
