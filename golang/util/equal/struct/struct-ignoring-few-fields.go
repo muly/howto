4c995adf3850a33d491a19f5bee40dbccfbb45fc
pk80 <praveen.goprog@gmail.com>
2021-01-03 17:02:23 +0530

    map[string]string comparision with cmp is done

--- golang/util/equal/struct/struct-ignoring-few-fields.go
+++ golang/util/equal/struct/struct-ignoring-few-fields.go
@@ -1,4 +1,4 @@
-// inspuired by a solution in https://stackoverflow.com/questions/47134293/compare-structs-except-one-field-golang/47134781
+// inspired by a solution in https://stackoverflow.com/questions/47134293/compare-structs-except-one-field-golang/47134781
 
 // need to extend this to cover
 //  pointers
@@ -14,15 +14,17 @@ import (
 
 	mapset "github.com/deckarep/golang-set"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 type Parent struct {
-	Name string
-	// NameP  *string
-	Date1  time.Time
-	Date2  time.Time
-	Family []string
-	Child  child
+	Name    string
+	NameP   *string
+	Date1   time.Time
+	Date2   time.Time
+	Family  []string
+	Child   child
+	Members map[string]string
 }
 
 type child struct {
@@ -42,9 +44,12 @@ func EqualExcept(f *Parent, other *Parent, ExceptField mapset.Set) bool {
 		value := val.Field(i)
 		otherValue := otherFields.FieldByName(typeField.Name)
 
-		if diff := cmp.Diff(value.Interface(), otherValue.Interface()); diff != "" {
-			return false
-		}
+		// if diff := cmp.Diff(value.Interface(), otherValue.Interface()); diff != "" {
+		// 	return false
+		// }
+		// or cmp.Equal(x, y, opt) // opt can be cmp.Comparer(func(x, y float64) bool {}
+		// or simply
+		return cmp.Equal(value.Interface(), otherValue.Interface())
 	}
 	return true
 }
@@ -52,28 +57,37 @@ func EqualExcept(f *Parent, other *Parent, ExceptField mapset.Set) bool {
 func main() {
 	n1 := "NNN"
 	n2 := "NNN"
-	// p1 := "PPP"
-	// p2 := "PPP"
+	p1 := "PPP"
+	p2 := "PPP2"
 
 	f1 := &Parent{
-		Name: n1,
-		// NameP:  &p1,
+		Name:   n1,
+		NameP:  &p1,
 		Date1:  time.Now(),
 		Date2:  time.Now(),
 		Family: []string{"1", "2"},
 		Child:  child{Address: "AAA"},
+		Members: map[string]string{
+			"brothers": "1",
+			"sisters":  "2",
+		},
 	}
 
 	f2 := &Parent{
-		Name: n2,
-		// NameP:  &p2,
+		Name:   n2,
+		NameP:  &p2,
 		Date1:  time.Now(),
 		Date2:  time.Now(),
 		Family: []string{"1", "2"},
 		Child:  child{Address: "AAA"},
+		Members: map[string]string{
+			"brothers": "1",
+			"sisters":  "2",
+		},
 	}
 
-	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("Date1", "Date2")))
-	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("Name")))
-
+	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("Date1", "Date2")))                                   // except time variables
+	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("Name")))                                             // except normal variable
+	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("NameP", "Date1", "Date2")))                          // except pointer and time variables
+	fmt.Println(EqualExcept(f1, f2, mapset.NewSet("NameP", "Date1", "Date2", "Members[\"brothers\"]"))) // except pointer and time variables
 }
