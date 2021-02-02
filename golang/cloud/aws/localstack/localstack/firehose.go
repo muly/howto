dd520df731b673de566d7def863236aaf16f2cfb
muly <msrinivasareddy@gmail.com>
2021-02-02 08:23:35 -0500

    firehose aws localstack test wip

---
+++ golang/cloud/aws/localstack/localstack/firehose.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	//"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/firehose"
+)
+
+var firehoseClinet *firehose.Firehose
+
+func Init() {
+	aws_access_key_id := os.Getenv("AWS_ACCESS_KEY_ID")
+	aws_secret_access_key := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	token := ""
+	creds := credentials.NewStaticCredentials(aws_access_key_id, aws_secret_access_key, token)
+	_, err := creds.Get()
+	if err != nil {
+		fmt.Printf("bad credentials: %s", err)
+	}
+
+	cfg := aws.NewConfig().WithRegion("us-east-1").WithCredentials(creds)
+
+	
+	firehoseClinet = firehose.New(session.New(), cfg) //TODO: session.New() is deprecated
+}
+
+
+func Handler(ctx context.Context, data events.KinesisEvent ) error {
+	input := firehose.PutRecordInput{
+		DeliveryStreamName: aws.String("test-hose1"),
+		Record: &firehose.Record{
+			Data : []byte(fmt.Sprintf("hello firehose: %v",time.Now() )),
+		},
+	}
+
+	_, err:= firehoseClinet.PutRecordWithContext(ctx,&input)
+	if err != nil{
+		return err
+	}
+
+	log.Print("Handler Done")
+	
+	return nil
+}
+
+func main() {
+	Init()
+
+	// lambda.Start(Handler)
+
+	err := Handler(context.TODO(), events.KinesisEvent{})
+	if err != nil{
+		log.Printf("Handler error,: %v", err)
+	}
+
+}
---
+++ golang/cloud/aws/localstack/localstack/go.mod
@@ -0,0 +1,17 @@
+module github.com/muly/howto/golang/cloud/aws/localstack/localstack
+
+go 1.14
+
+require (
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/aws/aws-lambda-go v1.22.0
+	github.com/aws/aws-sdk-go v1.37.2
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/containerd/continuity v0.0.0-20201208142359-180525291bb7 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/ory/dockertest v3.3.5+incompatible
+	github.com/sirupsen/logrus v1.7.0 // indirect
+)
